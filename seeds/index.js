78733c1952cb10c0d487d3ad9c0b3deae7031a9a
Revivedaniel <mlg.danielstark@gmail.com>
2022-12-02 23:03:02 -0800

    Added platform seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,13 +2,22 @@
 import db from "../config/connection.js";
 // use the gameData to insertMany
 import gameData from "./gameData.js";
-import { Game } from "../models/index.js";
+import platformData from "./platformData.js";
+import { Game, Platform } from "../models/index.js";
 
 db.once("open", async () => {
   try {
-    const platforms = await Game.insertMany(gameData);
-    console.log("success");
+    await Game.deleteMany({});
+    await Platform.deleteMany({});
+
+    const games = await Game.insertMany(gameData);
+    console.log("successfully seeded Games");
+    console.log(games);
+
+    const platforms = await Platform.insertMany(platformData);
+    console.log("successfully seeded Platforms");
     console.log(platforms);
+
     process.exit(0);
   } catch (err) {
     console.log(err);
