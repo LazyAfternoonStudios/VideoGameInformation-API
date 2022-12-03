f1ed2702cbb32df990d7da46dac76a5a839e468a
Revivedaniel <mlg.danielstark@gmail.com>
2022-12-02 23:12:02 -0800

    added 204 response for empty searches

--- routes/platforms/index.js
+++ routes/platforms/index.js
@@ -5,7 +5,11 @@ import { Platform } from "../../models/index.js";
 router.get("/", async (req, res) => {
   try {
     const platforms = await Platform.find({});
-
+    // if platforms is empty, return 204
+    if (platforms.length === 0) {
+      return res.status(204).json();
+    }
+    // if there are platforms, return 200
     res.status(200).json(platforms);
   } catch (err) {
     console.log(err);
@@ -14,10 +18,14 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:platformId", async (req, res) => {
-    const { platformId } = req.params;
+  const { platformId } = req.params;
   try {
-    const platforms = await Platform.find({platformId: platformId});
-
+    const platforms = await Platform.find({ platformId: platformId });
+    // if platforms is empty, return 204
+    if (platforms.length === 0) {
+      return res.status(204).json();
+    }
+    // if there are platforms, return 200
     res.status(200).json(platforms);
   } catch (err) {
     console.log(err);
