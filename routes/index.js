31f78f6801636551543258f6cf51e24dff131bba
Revivedaniel <mlg.danielstark@gmail.com>
2022-12-02 23:42:15 -0800

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const router = express.Router()
 
 import platformRoutes from './platforms/index.js'
 
-// TODO: Make routes semantic like /games, /platforms, /companies, etc.
-// Currently routes are divided into queries and mutations
-
 router.use('/platforms', platformRoutes);
 
 export default router;
