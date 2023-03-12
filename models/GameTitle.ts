b032059f740fd48fab12e6739ac41da1621c7b1a
Revivedaniel <mlg.danielstark@gmail.com>
2023-03-11 20:42:39 -0800

    Added gameGenerated and imageName for better search

--- models/GameTitle.ts
+++ models/GameTitle.ts
@@ -2,10 +2,14 @@ import { Schema, model, Document, Model } from 'mongoose';
 
 interface IGameTitle extends Document {
   title: string;
+  gameGenerated: boolean;
+  imageName: string;
 }
 
 const gameTitleSchema: Schema<IGameTitle> = new Schema({
-  title: { type: String, required: true }
+  title: { type: String, required: true },
+  gameGenerated: { type: Boolean, required: true, default: false },
+  imageName: { type: String, required: true, default: 'byp-new-game'}
 });
 
 // add text index on the 'title' field for text search
